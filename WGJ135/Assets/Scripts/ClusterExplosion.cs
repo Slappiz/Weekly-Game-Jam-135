03d61a434a7885d018f217750ffc3307d3b42564
Emil Gustafsson <emiltgustafsson@hotmail.com>
2020-02-08 19:45:10 +0100

    ClusterExplosion

---
+++ WGJ135/Assets/Scripts/ClusterExplosion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClusterExplosion : MonoBehaviour
+{
+    public GameObject projectilePrefab = null;
+    public bool destroyOnExplosion = true;
+    public float timeToDetonate = 1f;
+    public int numberOfProjectiles = 6;
+    public float radius = 1f;
+    public float projectileForce = 5f;
+    private void Start()
+    {
+        Invoke("Explode", timeToDetonate);
+    }
+
+    private void Explode()
+    {
+        float angleStep = 360f / numberOfProjectiles;
+        float angle = 0f;
+        
+        Vector2 startpos = new Vector2(this.transform.position.x, this.transform.position.y);
+        for (int i = 0; i < numberOfProjectiles; i++)
+        {
+            float DirX = startpos.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+            float DirY = startpos.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+            
+            Vector2 projectileVector =  new Vector2(DirX, DirY);
+            Vector2 projectileMoveDirection = (projectileVector - startpos).normalized * projectileForce;
+            
+            GameObject projectile = Instantiate(projectilePrefab);
+            projectile.transform.position = this.transform.position;
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(projectileMoveDirection.x, projectileMoveDirection.y);
+            angle += angleStep;
+            Debug.Log("cluster projectile spawned");
+        }   
+        if(destroyOnExplosion) Destroy(this.gameObject);
+    }
+}
--- WGJ135/Assets/Scripts/Shooter.cs
+++ WGJ135/Assets/Scripts/Shooter.cs
@@ -8,7 +8,6 @@ public class Shooter : MonoBehaviour
     public GameObject projectilePrefab = null;
     public float projectileForce = 20f;
     public float timeBetweenShots = 1f;
-
     private void Start()
     {
         InvokeRepeating("Shoot", 1, timeBetweenShots);
@@ -17,6 +16,7 @@ public class Shooter : MonoBehaviour
     void Shoot()
     {
         GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+        projectile.transform.localRotation = transform.localRotation;
         Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
         rb.AddForce(transform.up * projectileForce, ForceMode2D.Impulse);
         Destroy(projectile, 5f);
