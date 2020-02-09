248eb492e39af59ecdbea2e6c7fa04d59de14dac
Emil Gustafsson <emiltgustafsson@hotmail.com>
2020-02-09 11:55:03 +0100

    Ammunition

---
+++ WGJ135/Assets/Scripts/Cannon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public Star.STAR color;
+    public int health = 25;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ammunition")
+        {
+            Star.STAR ammoColor = other.gameObject.GetComponent<PlayerAmmunition>().color;
+            if (ammoColor == color)
+            {
+                TakeDamage();
+            }
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void TakeDamage()
+    {
+        health -= 1;
+        if(health <= 0) Destroy(this.gameObject);
+    }
+}
